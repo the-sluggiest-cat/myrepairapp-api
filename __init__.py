13403aa8d02c25319ec2c98bd0c1c103292741b2
scooglat <jaanov2013@gmail.com>
2025-11-19 12:41:33 -0500

    initial commit

---
+++ __init__.py
@@ -0,0 +1,6 @@
+"""## myrepairapp.py
+---
+
+Like `discord.py`, but designed from the ground up to work with [the MyRepairApp API](https://www.myrepairapp.com/api-docs/public-api).
+
+Heavy alpha. In fact, if you're reading this, get off of my computer. I've been working on this for too long to lose it all to a criminal."""
---
+++ exceptions.py
@@ -0,0 +1,19 @@
+class BaseMRAException(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+class TooManyRequests(BaseMRAException):
+    def __init__(self):
+        super().__init__("Too many requests.")
+
+class Forbidden(BaseMRAException):
+    def __init__(self):
+        super().__init__("This action is not allowed. Are you using the correct API key?")
+
+class InternalServerError(BaseMRAException):
+    def __init__(self):
+        super().__init__("MyRepairApp responded with status code 500. It may be down for maintenance, or something went wrong internally.")
+
+class BadRequest(BaseMRAException):
+    def __init__(self, message: str):
+        super().__init__(message)
