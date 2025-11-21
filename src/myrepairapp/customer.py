0901887408c358f521cd93d7b301d854d8bb738d
scooglat <jaanov2013@gmail.com>
2025-11-21 13:16:27 -0500

    added Customer -- eventually linking tickets to customers

--- src/myrepairapp/customer.py
+++ src/myrepairapp/customer.py
@@ -1,39 +1,53 @@
-import enum
-
-"""
-{
-  "id": "12345",
-  "firstName": "John",
-  "lastName": "Doe",
-  "company": "ACME Corp",
-  "primaryPhone": "123-456-7890",
-  "contactPhone": "123-456-7890",
-  "email": "john.doe@example.com",
-  "driversLicense": "123456789",
-  "storeCredit": 100.5,
-  "preferredContactMethods": ["email", "phone"],
-  "billingAgent": "John Smith",
-  "netTerms": "30 days",
-  "taxExempt": "No",
-  "postalCode": "12345",
-  "referralSourceId": null,
-  "street1": "123 Main St",
-  "street2": "Suite 100",
-  "country": "US",
-  "state": "CA",
-  "city": "Los Angeles"
-}
-"""
-
 class Customer:
-    customer_id             : int
-    firstName               : str
-    lastName                : str
-    company                 : str
-    primaryPhone            : str
-    contactPhone            : str
-    email                   : str
-    driversLicense          : int
-    storeCredit             : float
-    preferredContactMethods : list[str]
-    billingAgent            : str
+    customer_id               : int
+    first_name                : str
+    last_name                 : str
+    company                   : str
+    primary_phone             : str
+    contact_phone             : str
+    email                     : str
+    drivers_license           : int
+    store_credit              : float
+    preferred_contact_methods : list[str]
+    billing_agent             : str
+    net_terms                 : str
+    postal_code               : int
+    referral_source_id        : None
+    street1                   : str
+    street2                   : str
+    country                   : str
+    state                     : str
+    city                      : str
+
+    def __init__(self, customerId:int, firstName: str, lastName: str, company: str, primaryPhone: str, contactPhone: str, email: str, driversLicense: int, \
+                 storeCredit: float, preferredContactMethods: list[str], billingAgent: str, netTerms: str, postalCode: int, referralSourceId: str,         \
+                 street1: str, street2: str, country: str, state: str, city: str):
+        self.customer_id = customerId
+        self.first_name = firstName
+        self.last_name = lastName
+        self.company = company
+        self.primary_phone = primaryPhone
+        self.contact_phone = contactPhone
+        self.email = email
+        self.drivers_license = driversLicense
+        self.store_credit = storeCredit
+        self.preferred_contact_methods = preferredContactMethods
+        self.billing_agent = billingAgent
+        self.net_terms = netTerms
+        self.postal_code = postalCode
+        self.referral_source_id = referralSourceId
+        self.street1 = street1
+        self.street2 = street2
+        self.country = country
+        self.state = state
+        self.city = city
+
+    def __dict__(self):
+        return {"id": self.customer_id, "firstName": self.first_name, "lastName": self.last_name, "company": self.company, "primaryPhone": self.primary_phone,            \
+                "contactPhone": self.contact_phone, "email": self.email, "driversLicense": self.drivers_license, "storeCredit": self.store_credit,                        \
+                "preferredContactMethods": self.preferred_contact_methods if len(self.preferred_contact_methods) != 0 else None, "billingAgent": self.billing_agent,      \
+                "netTerms": self.net_terms, "postalCode": self.postal_code, "referralSourceId": self.referral_source_id, "street1": self.street1, "street2": self.street2,\
+                "country": self.country, "state": self.state, "city": self.city}
+    
+    def __repr__(self):
+        return f"{self.first_name} {self.last_name}"
